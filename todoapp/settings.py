6c55894b3f86daf030e6c7c0b64e060489be6188
codersiddhu <dwivedisiddharth1999@gmail.com>
2024-02-21 11:35:33 +0530

    new todo app with django and pgsql

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-5kibz)vh9l=lqmtf06+u*b!j!dh4ln6ph6j=gxc3zihixg8df@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app']
 
 
 # Application definition
